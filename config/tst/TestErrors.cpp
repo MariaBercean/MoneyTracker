5c06028de8a6e5e559542af4182e2654301ed047
DanielMalina <daniel_malina@yahoo.com>
2015-10-09 19:50:32 +0300

    merge

--- config/tst/TestErrors.cpp
+++ config/tst/TestErrors.cpp
@@ -1,12 +1,12 @@
 #include <Errors.h>
+#include <iostream>
 #include "gtest/gtest.h"
 
 TEST(ErrorTest, FileNameError)
 {
 	Error_C error;
-	error.SetErr( FILE_NAME_ERR );
-	ASSERT_EQ(, );
+	error.SetErr(FILE_NAME_ERR);
+	ASSERT_EQ(FILE_NAME_ERR, error.GetError());
 	
-
 }
 	
