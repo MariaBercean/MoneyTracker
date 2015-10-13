b67339aecfa5cbdadcc111f2691818ade070fc01
AmadeusCPPL <sascatalinraul@gmail.com>
2015-10-13 11:23:08 +0300

    lastentry

--- wallet/inc/Wallet.h
+++ wallet/inc/Wallet.h
@@ -8,19 +8,32 @@
 class Wallet
 {
 	private:
+		//stores the initial sum of a wallet
 		float initialSum_;
+		
+		//stores the current currency
 		std :: string currency_;
+		
+		//stores a vector of WalletEntry elements
+		//Input : a WalletEntry
 		std::vector<WalletEntry> walletList_; 
 	
 	public:
+		//Constructor with no arguments
 		Wallet();
-
+		
+		//Creates a new wallet file 
+		//Input : walletName , sign of the amount, amount
 		void createNewWalletFile(const std :: string &wallet, 
 								const char operation, const float amount);
 								
+		//Adds a WalletEntry to the walletList		
+		//Input : walletName , sign of the amount, amount
 		void addIncome(const std :: string &wallet, const char operation, 
 						const float amount);
 						
+		//check if a filename allready exists
+		//returns a bool 
 		bool fileExists (const std :: string &fileName);
 
 		//std :: string SplitFilename (const std :: string &str);
